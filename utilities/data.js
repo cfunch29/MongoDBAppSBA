acae96a618fe89910d29d634c96541be7dd34e82
cfunch29 <cfunch29@gmail.com>
2026-02-04 17:00:15 -0500

    added beverage data

---
+++ utilities/data.js
@@ -0,0 +1,84 @@
+export const wines = [
+  {
+    name: "Cabernet Sauvignon",
+    contents: ["Grapes"],
+    alcohol: true
+  },
+  {
+    name: "Pinot Noir",
+    contents: ["Grapes"],
+    alcohol: true
+  },
+  {
+    name: "Chardonnay",
+    contents: ["Grapes"],
+    alcohol: true
+  },
+  {
+    name: "Sauvignon Blanc",
+    contents: ["Grapes"],
+    alcohol: true
+  },
+  {
+    name: "Prosecco",
+    contents: ["Grapes"],
+    alcohol: true
+  }
+];
+
+export const mocktails = [
+  {
+    name: "Cucumber Mint Cooler",
+    contents: ["Cucumber", "Mint", "Lime", "Sparkling Water"],
+    alcohol: false
+  },
+  {
+    name: "Berry Basil Smash",
+    contents: ["Strawberries", "Blueberries", "Basil", "Lemon Juice"],
+    alcohol: false
+  },
+  {
+    name: "Pineapple Ginger Fizz",
+    contents: ["Pineapple Juice", "Ginger Beer", "Lime"],
+    alcohol: false
+  },
+  {
+    name: "Watermelon Lime Spritz",
+    contents: ["Watermelon", "Lime Juice", "Soda Water"],
+    alcohol: false
+  },
+  {
+    name: "Peach Iced Tea Mocktail",
+    contents: ["Peach", "Black Tea", "Lemon"],
+    alcohol: false
+  }
+];
+
+export const softDrinks = [
+  {
+    name: "Cola",
+    contents: ["Carbonated Water", "Sugar", "Natural Flavors", "Caffeine"],
+    alcohol: false
+  },
+  {
+    name: "Lemon Lime Soda",
+    contents: ["Carbonated Water", "Lemon Flavor", "Sugar"],
+    alcohol: false
+  },
+  {
+    name: "Orange Soda",
+    contents: ["Carbonated Water", "Orange Flavor", "Sugar"],
+    alcohol: false
+  },
+  {
+    name: "Root Beer",
+    contents: ["Carbonated Water", "Vanilla", "Sassafras Flavor"],
+    alcohol: false
+  },
+  {
+    name: "Ginger Ale",
+    contents: ["Carbonated Water", "Ginger Flavor", "Sugar"],
+    alcohol: false
+  }
+];
+
