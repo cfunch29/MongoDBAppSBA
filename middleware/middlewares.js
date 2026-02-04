66d4ea45de4184e018591fdcd8e86bb9033fbc26
cfunch29 <cfunch29@gmail.com>
2026-02-04 16:20:28 -0500

    added folders and middlewares

---
+++ middleware/middlewares.js
@@ -0,0 +1,11 @@
+export function logReq (req, res, next) {
+    console.log(`${req.method} -- ${req.url} -- ${new Date().toLocaleTimeString()} `);
+
+    if (req.body) console.table(req.body);
+
+    next();
+};
+
+export const globalErr = (err, _req, res, _next) => {
+    res.status(err.status || 500).json({ error: `❌ Error: ${err.message}`});
+};
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // imports 
+import express from "express";
+import dotenv from "dotenv";
 
 // setups 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // middleware 
+app.use(express.json());
+app.use(logReq);
 
 // routes 
 
 // global err handling middleware 
+app.use(globalErr);
 
 // listener 
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`)
+});
